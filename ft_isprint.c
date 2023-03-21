76e7021b811d45d20cdfcfb7bd76f0bb2ac5fc7a
osmanoscar <osmankok701@gmail.com>
2023-03-21 11:08:28 +0300

    fixed

--- ft_isprint.c
+++ ft_isprint.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ncolomer <ncolomer@student.42.fr>          +#+  +:+       +#+        */
+/*   By: okok <okok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 11:10:55 by ncolomer          #+#    #+#             */
 /*   Updated: 2019/10/10 00:23:05 by ncolomer         ###   ########.fr       */
